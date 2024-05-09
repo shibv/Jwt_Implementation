0829e5c44054bfe532d49df71525f5efcb0a3361
Shiv Shankar <ps431352@gmail.com>
2024-05-10 01:00:14 +0530

    Added .env file

--- api/index.js
+++ api/index.js
@@ -24,4 +24,4 @@ app.get('/', (req, res) => {
 
 app.use('/api', authRoutes);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'));
